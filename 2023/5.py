a8cfb47ada26266fa40368f8ff8b945142d1d3d5
Schiltenwolf <moritz.schiwo@gmail.com>
2023-12-06 09:23:04 +0100

    Update 5.py

--- 2023/5.py
+++ 2023/5.py
@@ -10,67 +10,136 @@ import regex as re
 def changeListDatatype(inputList, dataType=int):
     return list(map(dataType, inputList))
 
-with open("input\\input2.txt") as f:
+# with open("input\\input5.txt") as f:
+with open("input\\example.txt") as f:
     lines = [line.rstrip('\n') for line in f]
 
 # Part 1
 
-sumOfGames = 0
+seeds = changeListDatatype(re.findall(r'\d+', lines[0]))
+maps = []
+mapID = -1
 
-for x in range(len(lines)):
-    lines[x] = re.sub(r'(Game [0-9\(\)]+: )', ' ', lines[x])
-    red = re.findall(r'(\d+(?= red))', lines[x])
-    red = changeListDatatype(red)
-    maxRed = max(red)
+locations = []
 
+def mapper(seed):
+    step = 0
+    pos = seed    
+    while (step < len(maps)):
+        for y in range(0, len(maps[step])):
+            if (pos in range(maps[step][y][1], maps[step][y][1] + maps[step][y][2])):
+                pos = pos + (maps[step][y][0] - maps[step][y][1])
+                break
+        step += 1
 
-    green = re.findall(r'(\d+(?= green))', lines[x])
-    green = changeListDatatype(green)
-    maxGreen = max(green)
-    
-    blue = re.findall(r'(\d+(?= blue))', lines[x])
-    blue = changeListDatatype(blue)
-    maxBlue = max(blue)
-    
-    if(maxRed > 12 or maxGreen > 13 or maxBlue > 14):
-        print("Game " + str(x+1) + " is impossible")
-        print(maxRed)
-        print(maxGreen)
-        print(maxBlue)        
-        print(lines[x])
-    else:
-        print("Game " + str(x+1) + " can be played")
-        print(lines[x])
-        sumOfGames += (x+1)
+    return(pos)
 
+for x in range(1, len(lines)):
+    if(re.search(r'(map)', lines[x])):
+        maps.append([])
+        mapID += 1
+    elif(lines[x]):
+        maps[mapID].append(changeListDatatype(re.findall(r'\d+', lines[x])))
+        maps[mapID]
         
     
-print(sumOfGames)
+# for y in range(0, len(maps)):
+#     maps[y] = sorted(maps[y], key=lambda z: z[1])
+
+# for seed in range(0, len(seeds)):
+#     locations.append(mapper(seeds[seed]))
+    
+# print("Results Part 1: " + str(min(locations)))
+
 
 
 # Part 2
+seeds2 = []
+locations2 = []
+print(seeds)
 
-sumOfPowers = 0
+for i in range(0,len(seeds),2):
+    seeds2.append((seeds[i], seeds[i]+seeds[i+1]))
+    print(seeds2)
+  
+def mapper2(seed):
+    pos = []
+    for direction, start, length in maps:
+        if start <= seed < start + length:
 
-for x in range(len(lines)):
-    lines[x] = re.sub(r'(Game [0-9\(\)]+: )', ' ', lines[x])
-    red = re.findall(r'(\d+(?= red))', lines[x])
-    red = changeListDatatype(red)
-    maxRed = max(red)
+            pos.append(seed + direction - start)
+            break
+        else:
+            pos.append(seed)
+            
 
-
-    green = re.findall(r'(\d+(?= green))', lines[x])
-    green = changeListDatatype(green)
-    maxGreen = max(green)
+def mapper3(seeds):
     
-    blue = re.findall(r'(\d+(?= blue))', lines[x])
-    blue = changeListDatatype(blue)
-    maxBlue = max(blue)
-    
-    sumOfPowers += (maxRed*maxGreen*maxBlue)
-    
+    step = 0
+    while (step < len(maps)):
+        for y in range(0, len(maps[step])):
+            if (pos in range(maps[step][y][1], maps[step][y][1] + maps[step][y][2])):
+                pos = pos + (maps[step][y][0] - maps[step][y][1])
+                break
+        step += 1
 
-
+    return(pos)
+    
+    
+pos = []
+while len(seeds2) > 0:
+    lowerB, upperB = seeds2.pop()
+    for direction, start, length in maps:
+        overlapStart = max(lowerB, start)
+        overlapEnd = min(upperB, start + length)
+        if overlapStart < overlapEnd:
+            pos.append((overlapStart - start + direction, overlapEnd - start + direction))
+            if overlapStart < start:
+                seeds2.append((lowerB, overlapStart))
+            if overlapEnd < start + length:
+                seeds2.append((overlapEnd, upperB))
+            break
+    else:
+        pos.append((lowerB, upperB))
         
+            
+            
+# subArrays = seeds2
+# subMins = [None] * len(subArrays)
+
+# for s in range(0, 10):
+#     subrange = subArrays[s]
+#     print(subrange)
+#     loc = []
+#     for seed in subrange:
+#         loc.append(mapper(seed))
+#     subMins[s] = min(loc)
+
+# print(subMins)
+# minRange = subArrays[subMins.index(min(subMins))]
+# rangeLen = len(minRange)    
+
+# print("initial check finished:")
+# print(minRange)
+
+# while (rangeLen > 100000):
+#     subArrays = np.array_split(np.array(minRange),10)
+#     subMins = [None] * len(subArrays)
     
-print(sumOfPowers)
+#     for s in range(0, 10):
+#         sub = subArrays[s]
+#         subrange = range(min(sub), max(sub) + 1, 5000)
+#         print(subrange)
+#         loc = []
+#         for seed in subrange:
+#             loc.append(mapper(seed))
+#         subMins[s] = min(loc)
+    
+#     minRange = subArrays[subMins.index(min(subMins))]
+#     rangeLen = len(minRange)
+#     print("cycle finished")
+            
+# for seed in range(0, len(minRange)):
+#     locations2.append(mapper(minRange[seed]))
+            
+# print(min(locations2))
